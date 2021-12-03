1ebccca7bb24fb0587970ffc324a3e27d98f3672
DevSadik <wahidsadik88@gmail.com>
2021-12-03 23:50:06 +0600

    Project Upload

---
+++ app.js
@@ -0,0 +1,807 @@
+const student = [
+
+    {
+         
+        id      : 1,
+        name    :'Imran Sheikh',
+        group   :'B+',
+        adress  :'Bagerhat',
+        phone   :'01791160867',
+        batch   :'2018',
+        gender  :'Male',
+    
+        },
+        {
+             
+        id      : 2,
+        name    :'Morsalin Biswas',
+        group   :'A+',
+        adress  :'Potuakhali',
+        phone   :'01764656423',
+        batch   :'2018',
+        gender  :'Male',
+    
+        },
+        {
+             
+        id      : 3,
+        name    :'Md Shagor Hossen',
+        group   :'A+',
+        adress  :'Naogao',
+        phone   :'01706251349',
+        batch   :'2018',
+        gender  :'Male',
+    
+        },
+        {
+             
+        id      : 4,
+        name    :'Jesmin Akter',
+        group   :'B+',
+        adress  :'Khulna',
+        phone   :'01854282509',
+        batch   :'2018',
+        gender  :'Female',
+    
+        },
+        {
+             
+            id      : 5,
+            name    :'Atikur Rahman',
+            group   :'B+',
+            adress  :'Naogao',
+            phone   :'01748686033',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 6,
+            name    :'Hridoy KHan Shagor',
+            group   :'B+',
+            adress  :'Potuakhali',
+            phone   :'01757646490',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 7,
+            name    :'Shawon Sheikh',
+            group   :'B+',
+            adress  :'Khulna',
+            phone   :'01767589777',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 8,
+            name    :'Sumaiya Akter',
+            group   :'O+',
+            adress  :'Khulna',
+            phone   :'01985074701',
+            batch   :'2018',
+            gender  :'Female',
+        
+        },
+        {
+             
+            id      : 9,
+            name    :'Md Mehedi Hasan',
+            group   :'B+',
+            adress  :'Barishal',
+            phone   :'01745663207',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 10,
+            name    :'Sadia Islam Shanta',
+            group   :'A+',
+            adress  :'Khulna',
+            phone   :'01851105713',
+            batch   :'2018',
+            gender  :'Female',
+        
+        },
+        {
+             
+            id      : 11,
+            name    :'Abu Rayhan',
+            group   :'B+',
+            adress  :'Shatkhira',
+            phone   :'01768746068',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 12,
+            name    :'Fahim Shahriar',
+            group   :'A+',
+            adress  :'Pabna',
+            phone   :'01744628192',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 13,
+            name    :'Shourav Mia',
+            group   :'B+',
+            adress  :'Gaibandha',
+            phone   :'01788027702',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 13,
+            name    :'Wahid Sadik',
+            group   :'O-',
+            adress  :'Gaibandha',
+            phone   :'01703085993',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 14,
+            name    :'Al Amin',
+            group   :'O+',
+            adress  :'Gaibandha',
+            phone   :'01774180923',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 15,
+            name    :'Abdul Hakim',
+            group   :'B+',
+            adress  :'Pabna',
+            phone   :'01738048006',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 16,
+            name    :'Hasibul Hassan Shanto',
+            group   :'O+',
+            adress  :'Vola',
+            phone   :'01968475566',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 17,
+            name    :'Mehedi Hasan Fahim',
+            group   :'B+',
+            adress  :'Khulna',
+            phone   :'01943505173',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 18,
+            name    :'Md Mehrab Hossen',
+            group   :'O+',
+            adress  :'Kumilla',
+            phone   :'01906412002',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 19,
+            name    :'Arif Mia',
+            group   :'B+',
+            adress  :'Kishorganj',
+            phone   :'01772289100',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 20,
+            name    :'Robiul Islam',
+            group   :'AB+',
+            adress  :'Bogura',
+            phone   :'01708615663',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 21,
+            name    :'Rehnuma Afroz Ritu',
+            group   :'A+',
+            adress  :'Khulna',
+            phone   :'01928470874',
+            batch   :'2018',
+            gender  :'Female',
+        
+        },
+        {
+             
+            id      : 22,
+            name    :'Imam Hassan',
+            group   :'O+',
+            adress  :'JamalPur',
+            phone   :'01703355804',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 23,
+            name    :'Monir Hossain',
+            group   :'O+',
+            adress  :'Jamal Pur',
+            phone   :'01744428856',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 23,
+            name    :'Md Al Amin',
+            group   :'A-',
+            adress  :'Jamal Pur',
+            phone   :'01783178769',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 24,
+            name    :'Md Sabbir Hossain',
+            group   :'A+',
+            adress  :'Potuakhali',
+            phone   :'01751672611',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 25,
+            name    :'Sulaiman Akunji',
+            group   :'A+',
+            adress  :'Khulna',
+            phone   :'01919489942',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 26,
+            name    :'Shaheb Ali',
+            group   :'A+',
+            adress  :'Chuadanga',
+            phone   :'01909658849',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 27,
+            name    :'Supranta Haldar',
+            group   :'B+',
+            adress  :'Pirojpur',
+            phone   :'01760051378',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 28,
+            name    :'Md Imran Hossain',
+            group   :'O-',
+            adress  :'Vola',
+            phone   :'01732590645',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 29,
+            name    :'Taufiq Hassan',
+            group   :'O+',
+            adress  :'Shaiotpur',
+            phone   :'01701876130',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 30,
+            name    :'MST Suraiya Akter Priya',
+            group   :'A+',
+            adress  :'Gaibandha',
+            phone   :'01749873258',
+            batch   :'2018',
+            gender  :'Female',
+        
+        },
+        {
+             
+            id      : 31,
+            name    :'Umme Kulsum Ohi',
+            group   :'B+',
+            adress  :'JhalKathi',
+            phone   :'01724321518',
+            batch   :'2018',
+            gender  :'Female',
+        
+        },
+        {
+             
+            id      : 32,
+            name    :'Humayan Ahmed',
+            group   :'O+',
+            adress  :'KishorGanja',
+            phone   :'01775441616',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 33,
+            name    :'MD Rokon Uddin',
+            group   :'O+',
+            adress  :'LalmonirHat',
+            phone   :'01796061613',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 34,
+            name    :'Purnima Sarker',
+            group   :'A+',
+            adress  :'Norail',
+            phone   :'01926942627',
+            batch   :'2018',
+            gender  :'Female',
+        
+        },
+        {
+             
+            id      : 35,
+            name    :'Arafat Hossain',
+            group   :'B+',
+            adress  :'Ghoraghat',
+            phone   :'01750305384',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 36,
+            name    :'Morzina Khatun',
+            group   :'B+',
+            adress  :'Khulna',
+            phone   :'01991852941',
+            batch   :'2018',
+            gender  :'Female',
+        
+        },
+        {
+             
+            id      : 37,
+            name    :'Badhon Chandra Mohonto',
+            group   :'B+',
+            adress  :'Rangpure',
+            phone   :'01710253105',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 38,
+            name    :'Tonoya Iqbal',
+            group   :'O+',
+            adress  :'Dhaka',
+            phone   :'01638148310',
+            batch   :'2018',
+            gender  :'Female',
+        
+        },
+        {
+             
+            id      : 39,
+            name    :'Jahidul Islam Sabbir',
+            group   :'A+',
+            adress  :'Tangail',
+            phone   :'01734581824',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 39,
+            name    :'Al Amin Sheikh',
+            group   :'A+',
+            adress  :'Khulna',
+            phone   :'01939649050',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 40,
+            name    :'Md Abid Hassan',
+            group   :'AB+',
+            adress  :'Dinajpure',
+            phone   :'01701995942',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 41,
+            name    :'Mehedi Hassan Emon',
+            group   :'B+',
+            adress  :'Barguna',
+            phone   :'01779703992',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 41,
+            name    :'Anowar Hossain',
+            group   :'O+',
+            adress  :'Potuakhali',
+            phone   :'01759998587',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 42,
+            name    :'Omio Sami Khan',
+            group   :'O+',
+            adress  :'Tangail',
+            phone   :'01627767016',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 43,
+            name    :'Md Nure Alam',
+            group   :'O+',
+            adress  :'Potuakhali',
+            phone   :'01737238919',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 44,
+            name    :'Shamim Sheikh',
+            group   :'B+',
+            adress  :'Pabna',
+            phone   :'01750262792',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 45,
+            name    :'Rezaul Karim',
+            group   :'B+',
+            adress  :'Gazipure',
+            phone   :'01989454366',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 46,
+            name    :'Enamul Haque',
+            group   :'B+',
+            adress  :'Shirajganj',
+            phone   :'01721859600',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 47,
+            name    :'Emdadul Haque',
+            group   :'A+',
+            adress  :'Kushtia',
+            phone   :'01619915991',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 48,
+            name    :'Mehedi Hassan Shamim',
+            group   :'O+',
+            adress  :'Maymanshingh',
+            phone   :'01782575148',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 49,
+            name    :'Rabbi Hossain',
+            group   :'O+',
+            adress  :'Jhinaidah',
+            phone   :'01765372316',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 50,
+            name    :'Harun Hossen',
+            group   :'O+',
+            adress  :'Pabna',
+            phone   :'01704517671',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 51,
+            name    :'Abdul Barik',
+            group   :'O+',
+            adress  :'Natore',
+            phone   :'01710515745',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 52,
+            name    :'Jewel Mia',
+            group   :'A-',
+            adress  :'Manik Ganj',
+            phone   :'01881194750',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 53,
+            name    :'Aminul Islam',
+            group   :'O+',
+            adress  :'Shirajganj',
+            phone   :'01764761462',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 54,
+            name    :'Maruf Ahmed Pranto',
+            group   :'A+',
+            adress  :'Pabna',
+            phone   :'01766113940',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 55,
+            name    :'Sazzad Hossain',
+            group   :'B+',
+            adress  :'Jamalpur',
+            phone   :'01996630970',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 56,
+            name    :'Khalid Mahmud Pial',
+            group   :'O+',
+            adress  :'Khulna',
+            phone   :'01981300314',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 57,
+            name    :'Tajul Mustafa',
+            group   :'B+',
+            adress  :'Cox Bazar',
+            phone   :'01866813942',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 58,
+            name    :'Zulfikar Ali',
+            group   :'B+',
+            adress  :'Jhinaidha',
+            phone   :'01994217604',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 59,
+            name    :'Md Rifat Hossen',
+            group   :'O-',
+            adress  :'Jhinaidah',
+            phone   :'01786330836',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 60,
+            name    :'Ashraful Islam',
+            group   :'AB+',
+            adress  :'Khulna',
+            phone   :'01995578486',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 61,
+            name    :'Elias Hossen',
+            group   :'O+',
+            adress  :'Dhaka',
+            phone   :'01736332228',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 62,
+            name    :'Razib Mia',
+            group   :'B+',
+            adress  :'SherPur',
+            phone   :'01709777863',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 63,
+            name    :'Imran Hossen',
+            group   :'B+',
+            adress  :'Tangail',
+            phone   :'01884922474',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 64,
+            name    :'Jobayer Rahman',
+            group   :'AB+',
+            adress  :'Shirajganj',
+            phone   :'01750499590',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 65,
+            name    :'Ahsan Habib',
+            group   :'O+',
+            adress  :'Rangpure',
+            phone   :'01717856899',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 66,
+            name    :'Shojib Hossen',
+            group   :'A+',
+            adress  :'Shirajganj',
+            phone   :'01707754341',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+        {
+             
+            id      : 67,
+            name    :'Bashu Deb Pal',
+            group   :'B+',
+            adress  :'Tangail',
+            phone   :'01705091195',
+            batch   :'2018',
+            gender  :'Male',
+        
+        },
+
+
+]
+
+let Blood = prompt('Blood Group (O+)');
+let Place = prompt('Enter your place (Barishal)');
+
+student.map( data => {
+
+    if( data.group == Blood && data.adress == Place){
+
+        console.log(`
+
+        Batch   :   ${data.batch} 
+        Name    :   ${data.name} 
+        Blood   :   ${data.group}
+        Contact :   ${data.phone} 
+        Adress  :   ${data.adress} 
+        
+        
+        `);
+
+    }
+});
